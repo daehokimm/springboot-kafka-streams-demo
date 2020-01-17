6df1970cf1bc4d177faf2ffd57d25b13dff99c5d
daehokimm <daehokimm@gmail.com>
2020-01-17 16:16:49 +0900

    Supa Dupa Init Commit

---
+++ src/main/java/me/daehokimm/springbootkafkastreamsdemo/SpringbootKafkaStreamsDemoApplication.java
@@ -0,0 +1,13 @@
+package me.daehokimm.springbootkafkastreamsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaStreamsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaStreamsDemoApplication.class, args);
+	}
+
+}
